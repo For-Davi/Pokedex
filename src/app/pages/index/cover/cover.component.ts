0529ca8a47f42b6a6b831dec7ed070bf761816a7
Carlos Davi <carlosdavi004@gmail.com>
2023-05-22 13:32:25 -0300

    Adicioned AudioON- AudioOFF

--- src/app/pages/index/cover/cover.component.ts
+++ src/app/pages/index/cover/cover.component.ts
@@ -8,10 +8,25 @@ import { Router } from '@angular/router';
 })
 export class CoverComponent {
   
+  public audioVideo: boolean = true
+  public statusAudio : number = 1
+
   constructor(private router: Router){}
 
   goFeed():void{
     this.router.navigate(['feed'])
+  }
+
+  changeVolumeOn():void{
+    console.log('on');
+    this.audioVideo = false
+    this.statusAudio = 0
+  }
+
+  changeVolumeOff():void{
+    console.log('off');
+    this.audioVideo = true
+    this.statusAudio = 1
   }
 
 }
