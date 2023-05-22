2fe4622e53a5dc2e756e28c76416a4bef467ab6b
Carlos Davi <carlosdavi004@gmail.com>
2023-05-22 16:19:25 -0300

    Created Buttons Next e Previous Page List Pokemons

--- src/app/pages/feed/feed.module.ts
+++ src/app/pages/feed/feed.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupFeedComponent } from './group-feed/group-feed.component';
-
+import { TextComponent } from './components/text/text.component';
+import { SearchComponent } from './components/search/search.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { PokedexComponent } from './components/pokedex/pokedex.component';
+import { ListPokemonsComponent } from './components/list-pokemons/list-pokemons.component';
 
 
 @NgModule({
   declarations: [
-    GroupFeedComponent
+    GroupFeedComponent,
+    TextComponent,
+    SearchComponent,
+    FavoritesComponent,
+    PokedexComponent,
+    ListPokemonsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
   ]
 })
 export class FeedModule { }
